f7a7eec832a0a4cb59c6fa6ea6095616efc51db7
yangchengyu <158498806+yang-cheng-yu@users.noreply.github.com>
2025-04-12 20:45:41 -0400

    Nav now working

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,41 @@
 document.addEventListener("DOMContentLoaded", setupNav);
 
 function setupNav() {
+    const nav = document.createElement("nav");
+    nav.id = "nav";
+    nav.innerHTML = `
+        <span class="nav-item">
+            
+            <a href="/index.html">
+                Hadron
+                <!-- logo.png -->
+            </a>
+        </span>
+        <span class="nav-item">
+            <ul>
+                <li class="nav-item list-item"><a href="/html/productListing.html">Products</a></li>
+                <li class="nav-item list-item"><a href="/html/about.html">About</a></li>
+                <li class="nav-item list-item"><a href="/html/contact.html">Contact</a></li>
+            </ul>
+        </span>
+        <span>
+            <input type="text" name="searchBox" id="search-box" placeholder="Search apps..">
+        </span>
+        <span class="nav-item">
+            <div class="icon-link">
+                <a href="html/cart.html">
+                    <i class="bi bi-cart-fill"></i>
+                </a>
+            </div>
+            <div class="icon-link">
+                <a href="html/signIn.html">
+                    <i class="bi bi-person-circle"></i>
+                </a>
+            </div>
+        </span>
+    `;
+
+    if(document.body.firstChild) {
+        document.body.insertBefore(nav, document.body.firstChild)
+    }
 }
