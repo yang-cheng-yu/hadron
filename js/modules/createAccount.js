b8716d82e474b15e09d2ebc9c8ffb272812355d6
DavidSabb <158499251+DavidSabb@users.noreply.github.com>
2025-05-17 20:26:29 -0400

    adding created account to localstorage

--- js/modules/createAccount.js
+++ js/modules/createAccount.js
@@ -12,12 +12,16 @@ export function createAccount(event){
 
     localStorage.setItem('username', username, 'email', email, "password", password);
 
+    addAccounts(username,email,password);
 }
 
 function checkCredentials(){
     const error = document.getElementById("error-message");
     const regex = /[a-z0-9]+@[a-z]+\.[a-z]+$/;
     const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+
+    localStorage.checkUnique(email, username);
 
     if (regex.test(email) == false){
         error.textContent = "Invalid email Form";
@@ -40,4 +44,17 @@ function checkCredentials(){
     }
     error.textContent = "Account Created"
     return true;
+}
+
+function addAccounts(username, email, password) {
+    const accounts = JSON.parse(localStorage.getItem("accounts"));
+
+    const duplicate = accounts.find(acc => acc.email === email || acc.username === username);
+    if (duplicate) {
+        const errormsg = document.getElementById("error-message");
+        errormsg.textContent = "Username or email already exists";
+        return;
+    }
+    accounts.push({ username, email, password });
+    localStorage.setItem("accounts", JSON.stringify(accounts));
 }
