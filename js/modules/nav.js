74004e4afae3df6776787b9a45693f37cf003bf4
yangchengyu <158498806+yang-cheng-yu@users.noreply.github.com>
2025-05-21 10:51:51 -0400

    Github Pages Compatibility fix

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -8,30 +8,30 @@ export function setupNav() {
     const nav = document.createElement("nav");
     nav.id = "nav";
 
-    let location = (page === "index") ? "" : "./.";
+    let location = (page === "index") ? "" : "../";
     nav.innerHTML = `
         <div class="nav-item">
             
-            <a href="${location}./index.html">
+            <a href="${location}index.html">
                 <h1 class="logo"><span style="color: #FF3355">H</span>adron</h1>
                 <!-- logo.png -->
             </a>
         </div>
         <div class="nav-item nav-list">
             <ul>
-                <li class="nav-item list-item" id="products"><a href="${location}./pages/productListing.html">Products</a></li>
-                <li class="nav-item list-item" id="about"><a href="${location}./pages/about.html">About</a></li>
-                <li class="nav-item list-item" id="contact"><a href="${location}./pages/contact.html">Contact</a></li>
+                <li class="nav-item list-item" id="products"><a href="${location}pages/productListing.html">Products</a></li>
+                <li class="nav-item list-item" id="about"><a href="${location}pages/about.html">About</a></li>
+                <li class="nav-item list-item" id="contact"><a href="${location}pages/contact.html">Contact</a></li>
             </ul>
         </div>
         <div class="nav-item">
             <div class="icon-link">
-                <a id="cart" href="${location}./pages/cart.html">
+                <a id="cart" href="${location}pages/cart.html">
                     <i class="bi bi-cart-fill"></i>
                 </a>
             </div>
             <div class="icon-link">
-                <a href="${location}./pages/signIn.html">
+                <a href="${location}pages/signIn.html">
                     <i class="bi bi-person-circle"></i>
                 </a>
             </div>
